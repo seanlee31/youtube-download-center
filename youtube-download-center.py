136b2ce944163fb8421d3e262f1012702bef151c
Sean Lee <sean85326@hotmail.com>
2018-08-24 18:51:44 -0400

    [ADD] Move FFMPEG library to lib directory. [FIX] Fixed FFMPEG library not found on some platforms. [FIX] Fixed download_1080p_helper to provides better and more secure downloads for 1080p videos when downloading and merging audios and videos.

--- youtube-download-center.py
+++ youtube-download-center.py
@@ -15,16 +15,19 @@ from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
 
 import re
+import pprint
+import itertools
 ########################################
 ###              CODES               ###
 ########################################
 
 ### Video ###
 V_VP9_1080P = 248
+V_MP4_1080P = 137
 
 ### Audio ###
-A_MP4_128KB = 140
 A_OPUS_160KB = 251
+A_MP4_128KB = 140
 
 ### Video + Audio ###
 VA_MP4_720P = 22
@@ -58,7 +61,7 @@ class genericDownloaderThread(threading.Thread):
             print("[MAIN - {}] Missing Folder/Directory, {}, Has Been Created!\n".format(self.name, path))
     
 class videoDownloaderThread(genericDownloaderThread):
-    ## Concrete Product
+    ## Concrete Product (VIDEO)
     def __init__(self, yt_video_queue, init_num_videos, resolution, show_progress=False):
         super().__init__(yt_video_queue, init_num_videos, show_progress)
         self._resolution = resolution
@@ -128,60 +131,149 @@ class videoDownloaderThread(genericDownloaderThread):
                 print("[ERROR (RegexMatchError) - {} - {}/{}] YouTube Video {} Had a Problem Processing And Was Skipped!\n".format(self.name, self._count, self._init_quantity, yt_url))
                 self._count += 1
                 continue
-            except OSError:
-                print("[ERROR (OSError) - {} - {}/{}] YouTube Video {} Had a Problem Processing And Was Skipped!\n".format(self.name, self._count, self._init_quantity, yt_url))
-                self._count += 1
-                continue
+            # except OSError:
+            #     print("[ERROR (OSError) - {} - {}/{}] YouTube Video {} Had a Problem Processing And Was Skipped!\n".format(self.name, self._count, self._init_quantity, yt_url))
+            #     self._count += 1
+            #     continue
 
         print("[MAIN - {}] The Thread Has Finished Downloading All YouTube Videos Assigned!\nExiting Thread ... \n".format(self.name))
-        
-    def download_1080p_helper(self, yt, video_path):
-        print("[HELPER - {} - {}/{}] Start Downloading YouTube 1080P Video: {} ! \n".format(self.name, self._count, self._init_quantity, yt.watch_url))
-        itags = [V_VP9_1080P, A_OPUS_160KB]
-        file_names = list()
 
-        for itag in itags:      
-            video = yt.streams.get_by_itag(itag)
-            codec_type = (video.parse_codecs()[0] if video.parse_codecs()[0] is not None else video.parse_codecs()[1])
-            title = video.default_filename[:-5]
-            file_type = video.default_filename[-4:].upper()
-            filename = title + codec_type
-            file_names.append(filename)
-            print("[HELPER - {} - {}/{}] Start Downloading: {} @ {}. \n".format(self.name, self._count, self._init_quantity, filename, video_path)) 
-            video.download(output_path=video_path, filename=filename)
-            print("[HELPER - {} - {}/{}] Finished Downloading: {} @ {}. \n".format(self.name, self._count, self._init_quantity, filename, video_path))
-            
-        print("[HELPER - {} - {}/{}] Start Merging Audio & Video using ffmpeg. \n".format(self.name, self._count, self._init_quantity))
+    def get_ffmpeg_path(self):
+        if getattr(sys, 'frozen', False):
+            ## Running in a Pyinstaller Executable.
+            base_path = sys.executable
+            base_path = os.path.dirname(base_path)
+        else:
+            ## Running in a Normal Python Environment.
+            base_path = sys.path[0]
+
         ## Check Operating System
         if sys.platform == 'win32':
-            ffmpeg_path = os.path.join(sys.path[0], "ffmpeg", "win", "bin")
+            ffmpeg_path = os.path.join(base_path, "lib", "ffmpeg", "win")
         elif sys.platform == 'darwin':
-            ffmpeg_path = os.path.join(sys.path[0], "ffmpeg", "mac", "bin")
+            ffmpeg_path = os.path.join(base_path, "lib", "ffmpeg", "mac")
         elif sys.platform.startswith('linux'):
-            ffmpeg_path = os.path.join(sys.path[0], "ffmpeg", "linux", "bin")
+            ffmpeg_path = os.path.join(base_path, "lib", "ffmpeg", "linux")
         
-        os.chdir(video_path)
-        input_files = ['{}.{}'.format(file_names[0], file_type), '{}.{}'.format(file_names[1], file_type)]
-        subprocess.call([os.path.join(ffmpeg_path, "ffmpeg"), 
-        '-i', input_files[0], 
-        '-i', input_files[1], 
-        '-c:v', 'copy',
-        '-c:a', 'aac', 
-        '-ac', '2',
-        '-ar', '48000', 
-        '-ab', '160k',
-        '-f', 'matroska',
-        '-y',  ## overwrite if the output video has already existed.
-        '{}.mkv'.format(title)])
+        ffmpeg_path = os.path.join(ffmpeg_path, "bin", "ffmpeg")
 
-        for f in input_files:
-            os.remove(f)
-            print("Removed {}. \n".format(f))
+        return ffmpeg_path
+
+    def get_available_1080p_codecs(self, yt):
+        def remove_none_combinations_filter(comb):
+            if comb is None:
+                return False
+            else:
+                return True
+
+        def check_combinations_filter(comb):
+            if (yt.streams.get_by_itag(comb[0]) is not None) and (yt.streams.get_by_itag(comb[1]) is not None):
+                return comb
+            else:
+                return None
+
+        def get_available_1080p_codecs_helper(combination):
+            return list(map(check_combinations_filter, combination))
+
+        video_codec_tags = [V_VP9_1080P, V_MP4_1080P]
+        audio_codec_tags = [A_OPUS_160KB, A_MP4_128KB]
+        
+        combinations = list(itertools.product(video_codec_tags, audio_codec_tags))
+        opus_audio_combinations = list()
+        mp4_audio_combinations = list()
+
+        for comb in combinations:
+            if A_OPUS_160KB in comb:
+                opus_audio_combinations.append(comb)
+            elif A_MP4_128KB in comb:
+                mp4_audio_combinations.append(comb)
+        
+        available_combinations = list()
+        ## Check Availabilities
+        opus_audio_combinations = get_available_1080p_codecs_helper(opus_audio_combinations)
+        mp4_audio_combinations = get_available_1080p_codecs_helper(mp4_audio_combinations)
+
+        ### Push in all combinations, including the None ones resulted from get_available_1080p_codecs_helper().
+        available_combinations.extend(opus_audio_combinations)
+        available_combinations.extend(mp4_audio_combinations)
+
+        ### Filter out the None objects
+        available_combinations = list(filter(remove_none_combinations_filter, available_combinations))
+        
+        return available_combinations
+
+    def get_title_and_file_ext(self, filename):
+        if filename.endswith('.webm'):
+            title = filename[:-5]
+            file_ext = filename[-4:].upper()
+        elif filename.endswith('.mp4'):
+            title = filename[:-4]
+            file_ext =  filename[-3:].upper()
+        return title, file_ext
+
+    def download_1080p_helper(self, yt, video_path):
+        available_combinations = self.get_available_1080p_codecs(yt)
+        ## Select the first/best quality combination.
+        file_names = list()
+        video_itag = available_combinations[0][0]
+        audio_itag = available_combinations[0][1]
+
+        ## Start Downloads
+        ### Download Video Part
+        video = yt.streams.get_by_itag(video_itag)
+        codec_type = "VIDEO"
+        title, file_ext = self.get_title_and_file_ext(video.default_filename)
+        filename = title + codec_type
+        file_names.append(filename)
+        print("[HELPER - {} - {}/{}] Start Downloading YouTube 1080P - Video: {} ! \n".format(self.name, self._count, self._init_quantity, yt.watch_url))
+        video.download(output_path=video_path, filename=filename)
+
+        print("[HELPER - {} - {}/{}] Finished Downloading YouTube 1080P - Video: {} ! ".format(self.name, self._count, self._init_quantity, filename))
+        print("[HELPER - {} - {}/{}] Saved Directory: {}. \n".format(self.name, self._count, self._init_quantity, video_path))
+
+        ### Download Audio Part
+        audio = yt.streams.get_by_itag(audio_itag)
+        codec_type = "AUDIO"
+        title, file_ext = self.get_title_and_file_ext(audio.default_filename)
+        filename = title + codec_type
+        file_names.append(filename)
+        print("[HELPER - {} - {}/{}] Start Downloading YouTube 1080P - Audio: {} ! \n".format(self.name, self._count, self._init_quantity, yt.watch_url))
+        audio.download(output_path=video_path, filename=filename)
+
+        print("[HELPER - {} - {}/{}] Finished Downloading YouTube 1080P - Audio: {} ! ".format(self.name, self._count, self._init_quantity, filename))
+        print("[HELPER - {} - {}/{}] Saved Directory: {}. \n".format(self.name, self._count, self._init_quantity, video_path))
+
+        ## Start Merging Audio & Video
+        print("[HELPER - {} - {}/{}] Start Merging Audio & Video Using FFMPEG: {} ! \n".format(self.name, self._count, self._init_quantity, title))
+        os.chdir(video_path)
+        input_files = ['{}.{}'.format(file_names[0], file_ext), '{}.{}'.format(file_names[1], file_ext)]
+        ffmpeg_path = self.get_ffmpeg_path()
+        print(ffmpeg_path)
+        try:
+            subprocess.call([ffmpeg_path, 
+            '-i', input_files[0], 
+            '-i', input_files[1], 
+            '-c:v', 'copy',
+            '-c:a', 'aac', 
+            '-ac', '2',
+            '-ar', '48000', 
+            '-ab', '160k',
+            '-f', 'matroska',
+            '-y',  ## overwrite if the output video has already existed.
+            '{}.mkv'.format(title)])
+
+            for f in input_files:
+                os.remove(f)
+                print("Removed {}. \n".format(f))
+
+        except FileNotFoundError:
+            print("[ERROR (FileNotFoundError) - {} - {}/{}] FFMPEG Library Is Missing. \n".format(self.name, self._count, self._init_quantity))
+            pass
 
         print("[HELPER - {} - {}/{}] Finished Downloading and Merging Audio & Video for 1080p YouTube Video: {}. \n".format(self.name, self._count, self._init_quantity, title))
 
 class audioDownloaderThread(genericDownloaderThread):
-    ## Concrete Product
+    ## Concrete Product (AUDIO)
     def __init__(self, yt_audio_queue, init_num_audios, show_progress=False):
         super().__init__(yt_audio_queue, init_num_audios, show_progress)
 
@@ -216,6 +308,7 @@ class audioDownloaderThread(genericDownloaderThread):
         print("[MAIN - {}] The Thread Has Finished Downloading All YouTube Videos Assigned!\nExiting Thread ... \n".format(self.name))
 
 class YTDC_GUI():
+    ## YouTube Download Center GUI
     def __init__(self, current_version):
         self._version = current_version
         self.root = Tk()
@@ -224,21 +317,21 @@ class YTDC_GUI():
         self.label_mode = Label(self.root, text='Select Mode (AUDIO/VIDEO): ', font='Helvetica 10 bold')
 
         mode_options = ("AUDIO", "VIDEO")
-        self.cbox_mode = ttk.Combobox(self.root, values=mode_options, width=40, font='Helvetica 8 bold')
+        self.cbox_mode = ttk.Combobox(self.root, justify='center',values=mode_options, width=40, font='Helvetica 8 bold')
         self.label_num_threads = Label(self.root, text='Select # of Thread Using: ', font='Helvetica 10 bold')
 
         thread_options = tuple([str(i) for i in range(1, 10+1)])
-        self.cbox_thread = ttk.Combobox(self.root, values=thread_options, width=40, font='Helvetica 8 bold')
+        self.cbox_thread = ttk.Combobox(self.root, justify='center', values=thread_options, width=40, font='Helvetica 8 bold')
 
         self.label_resolution = Label(self.root, text='Select Resolution (If VIDEO Mode): ', font='Helvetica 10 bold')
 
         resolution_options = ('1080P', '720P')
-        self.cbox_resolution = ttk.Combobox(self.root, values=resolution_options, width=40, font='Helvetica 8 bold')
+        self.cbox_resolution = ttk.Combobox(self.root, justify='center', values=resolution_options, width=40, font='Helvetica 8 bold')
 
         self.label_warning = Label(self.root, text='You Will Be Able To Cancel The Downloading Process By Clicking "EXIT" Button.', font='Helvetica 10 bold', fg="red")
 
         ## Define DOWNLOAD Button
-        self.download_button = Button(self.root, text="START DOWNLOAD", command=self.main, width=50, font='Helvetica 8 bold', cursor="hand2")
+        self.download_button = Button(self.root, text="START DOWNLOAD", command=self.download, width=50, font='Helvetica 8 bold', cursor="hand2")
         
         ## Define EXIT Button
         self.exit_button = Button(self.root, text="EXIT", width=50, command=self.root.destroy, font='Helvetica 8 bold', cursor="hand2")
@@ -250,42 +343,6 @@ class YTDC_GUI():
         self.label_github = Label(self.root, text='[Github] https://github.com/seanlee31/youtube-download-center', font='Helvetica 8 bold', fg="blue", cursor="hand2")
         self.label_bug_report = Label(self.root, text='[BUG Report] https://github.com/seanlee31/youtube-download-center/issues', font='Helvetica 8 bold', fg="blue", cursor="hand2")
 
-    def run(self):
-        self.root.title("YouTube Download Center v{}".format(self._version))
-        self.label_pl_urls.grid(row=0, column=0, columnspan=2, pady=(10, 10))
-
-        self.text_pl_urls.grid(row=1, column=0, columnspan=2)
-
-        self.label_mode.grid(row=2, column=0)
-        
-        self.cbox_mode.current(1)
-        self.cbox_mode.grid(row=2, column=1)
-
-        self.label_num_threads.grid(row=3, column=0)
-
-        self.cbox_thread.current(4)
-        self.cbox_thread.grid(row=3, column=1)
-
-        self.label_resolution.grid(row=4, column=0)
-
-        self.cbox_resolution.current(0)
-        self.cbox_resolution.grid(row=4, column=1)
-
-        self.label_warning.grid(row=5, column=0, columnspan=2, pady=(10, 10))
-
-        self.download_button.grid(row=6, column=0, columnspan=2)
-
-        self.exit_button.grid(row=7, column=0, columnspan=2)
-
-        self.label_author.grid(row=8, column=0, columnspan=2, pady=(10, 0))
-
-        self.label_github.grid(row=9, column=0, columnspan=2)
-        self.label_bug_report.grid(row=10, column=0, columnspan=2)
-
-        ## Start GUI
-        self.root.mainloop()
-
-    
     def get_yt_urls_and_paths(self, pl_urls_and_paths):
         yt_urls = list()
         pl_paths = list()
@@ -334,7 +391,7 @@ class YTDC_GUI():
                 diff = num_mappings_per_thread - num_threads
         
         # yt_downloaders = list()
-        ## Start DownloaderThread workers.
+        ## Start DownloaderThread Workers.
         for mappings in mappings_by_threads:
             mappings = deque(mappings)
             num_mappings = len(mappings)
@@ -364,7 +421,7 @@ class YTDC_GUI():
                 self.download_button['state'] = 'normal'
                 self.exit_button['state'] = 'normal'
 
-    def main(self):
+    def download(self):
         ## Disable buttons
         self.download_button['state'] = 'disabled'
 
@@ -397,7 +454,43 @@ class YTDC_GUI():
         # buttonMonitorThread.run()
         # print("[MAIN] All Threads Had Successfully Finished Downloading Quests! \n")
 
+    def run(self):
+        self.root.title("YouTube Download Center v{}".format(self._version))
+        self.label_pl_urls.grid(row=0, column=0, columnspan=2, pady=(10, 10))
+
+        self.text_pl_urls.grid(row=1, column=0, columnspan=2)
+
+        self.label_mode.grid(row=2, column=0)
+        
+        self.cbox_mode.current(1)
+        self.cbox_mode.grid(row=2, column=1)
+
+        self.label_num_threads.grid(row=3, column=0)
+
+        self.cbox_thread.current(4)
+        self.cbox_thread.grid(row=3, column=1)
+
+        self.label_resolution.grid(row=4, column=0)
+
+        self.cbox_resolution.current(0)
+        self.cbox_resolution.grid(row=4, column=1)
+
+        self.label_warning.grid(row=5, column=0, columnspan=2, pady=(10, 10))
+
+        self.download_button.grid(row=6, column=0, columnspan=2)
+
+        self.exit_button.grid(row=7, column=0, columnspan=2)
+
+        self.label_author.grid(row=8, column=0, columnspan=2, pady=(10, 0))
+
+        self.label_github.grid(row=9, column=0, columnspan=2)
+        self.label_bug_report.grid(row=10, column=0, columnspan=2)
+
+        ## Start GUI
+        self.root.mainloop()
+
+
 if __name__ == '__main__':
-    current_version = "1.2.0"
+    current_version = "1.2.1"
     YTDC_GUI = YTDC_GUI(current_version=current_version)
     YTDC_GUI.run()
